78abf719dbeb27872100d05328e6a1faa3e564e2
Unknown <np.carlos@gmail.com>
2019-09-23 02:16:27 -0500

    Creación del esqueleto de Nueva Orden

--- src/app/pages/order/order-new/order-new.page.ts
+++ src/app/pages/order/order-new/order-new.page.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderNewPage implements OnInit {
 
+  private customers: any;
+  
+  private customer: any;
+  
+  private resultado: any;
+  
   constructor() { }
 
   ngOnInit() {
+      this.customers = [];
   }
 
 }
