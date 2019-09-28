012973470a584f7e7e9f905d69c26e72cf337b82
Juan Felipe Torres <jf.torresp@uniandes.edu.co>
2019-09-28 16:19:17 -0500

    Creación del componente new para la página de prelot-request

---
+++ src/app/pages/prelot-request/prelot-request-new/prelot-request-new.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prelot-request-new',
+  templateUrl: './prelot-request-new.page.html',
+  styleUrls: ['./prelot-request-new.page.scss'],
+})
+export class PrelotRequestNewPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
