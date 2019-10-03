3a8e2f46dd3a35cf2c54c24ac8f4b2cf1378d646
Fabián Pallares <fabianpallares23@gmail.com>
2019-10-03 05:00:26 -0500

    Client: Creación de páginas / botón new/ link menú

---
+++ src/app/pages/client/client-new/client-new.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-new',
+  templateUrl: './client-new.page.html',
+  styleUrls: ['./client-new.page.scss'],
+})
+export class ClientNewPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/client/client-update/client-update.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-client-update',
+  templateUrl: './client-update.page.html',
+  styleUrls: ['./client-update.page.scss'],
+})
+export class ClientUpdatePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
