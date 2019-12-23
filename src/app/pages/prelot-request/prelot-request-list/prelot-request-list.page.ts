c397e48bbf0ba49f50a665645b731650be0ee910
Carlos <np.carlos@gmail.com>
2019-12-23 12:58:40 -0500

    Modificación de Prelot-Request-List Page

--- src/app/pages/prelot-request/prelot-request-list/prelot-request-list.page.ts
+++ src/app/pages/prelot-request/prelot-request-list/prelot-request-list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from 'src/app/api/services/rest-api.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-prelot-request-list',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prelot-request-list.page.scss'],
 })
 export class PrelotRequestListPage implements OnInit {
+ 
+  private endPoint:string = "prelot_requests";
+  
+  private prelotRequests: any;
 
-  constructor() { }
+  constructor(
+    public api: RestApiService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.getRequests();
   }
 
+  async getRequests() {
+    
+    await this.api.get(this.endPoint)
+     .subscribe(res => {
+       console.log(res);
+       if(res.success)
+       {
+         this.prelotRequests = res.data;
+       }
+       else
+       {
+         console.log(res);
+       }
+       //loading.dismiss();
+     }, err => {
+       console.log(err);
+       //loading.dismiss();
+     });
+ }
+
+ viewDetails(item)
+ {
+  this.router.navigate(['/prelot-request-details', {request: JSON.stringify(item)}]);
+ }
+
+ marcarComoEmpacado(item)
+ {
+
+ }
 }
