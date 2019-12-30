dba556926f4b71af101d1105e8c6ff0b5e3e9f55
Carlos <np.carlos@gmail.com>
2019-12-30 18:59:06 -0500

    Diseño de la página Prelot-Request-Details

--- src/app/pages/prelot-request/prelot-request-details/prelot-request-details.page.ts
+++ src/app/pages/prelot-request/prelot-request-details/prelot-request-details.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { RestApiService } from 'src/app/api/services/rest-api.service';
+import { PrelotRequestService } from 'src/app/api/services/prelot-request.service';
+
+import { Plugins } from '@capacitor/core';
+import { Router } from '@angular/router';
+
+const { Modals } = Plugins;
 
 @Component({
   selector: 'app-prelot-request-details',
@@ -8,23 +15,46 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PrelotRequestDetailsPage implements OnInit {
 
+
   private prelotRequest:any;
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    public api: RestApiService,
+    public prelotRequestService: PrelotRequestService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     let requestInfo = this.activatedRoute.snapshot.paramMap.get('request');
     if( requestInfo != null &&  requestInfo != undefined && requestInfo != "")
     {
-      console.log("prelot deails");
       this.prelotRequest = JSON.parse(requestInfo);
-      console.log(this.prelotRequest);
     }
     else{
       console.log("No hay nada");
     }
   }
 
+  cambiarEstadoPrelotSolicitar()
+  {
+    this.prelotRequestService.cambiarEstadoPrelotSolicitar(this.prelotRequest).then(data =>{
+      this.router.navigate(['/prelot-request-list']);
+    });
+  }
+
+  cambiarEstadoPrelotSolicitarYEmpacar()
+  {
+    this.prelotRequestService.cambiarEstadoPrelotSolicitarYEmpacar(this.prelotRequest).then(data =>{
+      this.router.navigate(['/prelot-request-list']);
+    });
+  }
+
+  async eliminarPrelot()
+  {
+    let alertRet = await Modals.alert({
+      title: 'Error',
+      message: "No implementado aún"
+    });
+  }
 }
