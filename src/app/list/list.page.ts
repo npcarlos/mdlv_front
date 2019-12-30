458cb42cf9116182b6d0ff0417c9cf8a120b111f
Carlos <np.carlos@gmail.com>
2019-12-30 18:56:41 -0500

    Eliminación de comentarios innecesarios

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -31,7 +31,30 @@ export class ListPage implements OnInit {
   }
 
   ngOnInit() {
+
+
   }
+/*
+  async getData() {
+    try {
+      const url = 'http://vtry-on.com/mdlv-dev/public/api/';
+      const params = {};
+      const headers = {};
+
+      const response = await this.http.get(url, params, headers);
+
+      console.log(response.status);
+      console.log(JSON.parse(response.data)); // JSON data returned by server
+      console.log(response.headers);
+
+    } catch (error) {
+      console.error(error.status);
+      console.error(error.error); // Error message as string
+      console.error(error.headers);
+    }
+  }
+
+*/
   // add back when alpha.4 is out
   // navigate(item) {
   //   this.router.navigate(['/list', JSON.stringify(item)]);
