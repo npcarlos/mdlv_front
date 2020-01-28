bc4d59d2f3999a6af4e8d8218146da1e925c124c
Carlos <np.carlos@gmail.com>
2020-01-28 11:53:15 -0500

    Configuración de Android y copia de seguridad de splash e íconos

---
+++ bin/setNameDev.js
@@ -0,0 +1,28 @@
+console.log("Ejecutando script");
+
+var fs = require('fs'),
+    xml2js = require('xml2js');
+ 
+var parser = new xml2js.Parser();
+fs.readFile(__dirname + '/../android/app/src/main/res/values/strings.xml', function(err, data) {
+    parser.parseString(data, function (err, result) {
+        let strings = result.resources.string;
+        strings.forEach(stringTag => {
+            if(stringTag["$"].name == "title_activity_main")
+            {
+                stringTag["_"] = "DEEEEV";
+            }
+        });
+        console.dir(JSON.stringify(result));
+        var builder = new xml2js.Builder();
+        var xml = builder.buildObject(result);
+        console.log("XML NUEVO");
+        console.log(xml);
+        fs.writeFile(__dirname + '/../android/app/src/main/res/values/strings.xml', xml, function(errW, dataW){
+            console.log("archivo generado");
+        });
+        console.log('Done');
+    });
+});
+
+console.log("Cambio de nombre realizado");
